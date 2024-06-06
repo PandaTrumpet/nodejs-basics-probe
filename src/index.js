615ef723164c50fc786b4abaec25a8044d7c428f
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-06 12:00:53 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-const message = 'Hello Pasdssss';
-console.log(message);
+// const message = 'Hello Pasdssss';
+// console.log(message);
+import fs from 'node:fs/promises';
+import path from 'node:path';
+// --------------------Sync-------------------
+// console.log(content);
+// console.log(content.toString('utf-8'));
+
+const workdir = path.join(process.cwd());
+
+// // const somePath = path.join(workdir, 'src', 'index.js');
+// // console.log(somePath);
+const filePath = path.join(workdir, 'text.txt');
+// const content = fs.readFileSync(filePath);
+// console.log(content.toString());
+
+// ------------------------Callback------------------
+// fs.readFile(filePath, (error, data) => {
+//   fs.writeFile('src/output.txt', 'Panda', (er) => {
+//     console.log('write');
+//   });
+//   console.log('data', data.toString());
+// });
+
+// --------------------------Asyn----------------------
+const content = await fs.readFile(filePath);
+console.log(content.toString());
